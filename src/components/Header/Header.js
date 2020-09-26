8880d93d6a6038bc4a7bd2bc7bf52c86c5be58e3
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-26 10:35:17 +0200

    styled header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './style.css';
+
 function Header({ score }) {
   return (
-    <div>
+    <div className="header">
       <span>SCORE: {score}</span>
     </div>
   );
