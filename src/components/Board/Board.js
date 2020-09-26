295afb20b72d557dbf80483d74deec667e59ce0f
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-26 13:52:08 +0200

    before eat

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,13 +6,58 @@ import Food from '../Food';
 import './style.css';
 
 class Board extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.pacmanRef = React.createRef();
+
+    this.foods = [];
+    this.amountOfFood = (
+      (window.innerWidth - this.props.foodSize)
+      * (window.innerHeight - this.props.topScoreBoardHeight)
+    ) / (this.props.foodSize * this.props.foodSize) - 1;
+
+    console.log(this.amountOfFood);
+    for (let i = 0; i < this.amountOfFood; i++) {
+      this['food' + i] = React.createRef();
+    }
+  }
+
+  lookForEat = () => {
+    // const 
+  }
+
   render() {
+    const { foodSize, border, topScoreBoardHeight } = this.props;
+    let foods = [];
+    let currentTop = 0;
+    let currentLeft = 1 * foodSize;
+    for (let i = 0; i < this.amountOfFood; i++) {
+      if (currentLeft + foodSize >= window.innerWidth - border) {
+        currentTop += this.props.foodSize;
+        currentLeft = 0;
+      }
+
+      if (currentTop + foodSize >= (window.innerHeight - border - topScoreBoardHeight )) {
+        break;
+      }
+
+      const position = { left: currentLeft, top: currentTop };
+      currentLeft += foodSize;
+      foods.push(
+        <Food
+          key={`food-elem-${i}`}
+          position={position}
+          ref={this['food' + i]}
+        />
+      );
+    }
+
     return (
       <div className="board">
-        <Food position={{top: 100, left: 100 }} />
-        <Food position={{top: 50, left: 100 }} />
-        <Food position={{top: 250, left: 100 }} />
-        <Pacman />
+        {foods}
+        <Pacman ref={this.pacmanRef} />
         {/* <Ghost color="yellow" />
         <Ghost color="red" />
         <Ghost color="pink" /> */}
@@ -20,6 +65,13 @@ class Board extends Component {
       </div>
     )
   }
+}
+
+// TODO: refactor and move to config
+Board.defaultProps = {
+  foodSize: 50,
+  border: 10 * 2,
+  topScoreBoardHeight: 50
 }
 
 export default Board;
