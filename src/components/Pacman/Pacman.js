b36621114635dde8c71c645e9786921339143c1d
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-26 11:44:49 +0200

    moving pacman part 1

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -7,8 +7,8 @@ class Pacman extends Component {
   state = {
     direction: 'right',
     position: {
-      top: 50,
-      left: 50
+      top: 0,
+      left: 0
     }
   }
 
@@ -23,6 +23,31 @@ class Pacman extends Component {
 
   handleKeyDown = (event) => {
     console.log(event.keyCode, event.key);
+
+    const currentTop = this.state.position.top;
+    const currentLeft = this.state.position.left;
+
+    // 39 ArrowRight
+    // 40 ArrowDown
+    // 37 ArrowLeft
+    // 38 ArrowUp
+    if (event.key === 'ArrowUp') {
+      this.setState({
+        direction: 'up'
+      });
+    } else if (event.key === 'ArrowRight') {
+      this.setState({
+        direction: 'right'
+      });
+    } else if (event.key === 'ArrowDown') {
+      this.setState({
+        direction: 'down'
+      });
+    } else if (event.key === 'ArrowLeft') {
+      this.setState({
+        direction: 'left'
+      });
+    }
   }
 
   render() {
