659709134dd2aa361fc2e275e05044db63e4b5ab
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-26 13:10:14 +0200

    moving ghost

--- src/components/Ghost/Ghost.js
+++ src/components/Ghost/Ghost.js
@@ -14,23 +14,63 @@ class Ghost extends Component {
 
   componentDidMount() {
     this.changeDirectionInterval = setInterval(this.changeDirection, 2000);
+    this.moveInterval = setInterval(this.move, 2000);
   }
 
   componentWillUnmount() {
     clearInterval(this.changeDirectionInterval);
+    clearInterval(this.moveInterval);
   }
 
   changeDirection = () => {
     const arrayOfMovement = ['left', 'up', 'down', 'right'];
     const movement = Math.floor(Math.random() * 4);
 
-    this.setState({ direction: arrayOfMovement[movement] }, () => {
-      console.log('direction: ', this.state.direction)
-    });
+    this.setState({ direction: arrayOfMovement[movement] });
   }
 
   move = () => {
-    
+    // TODO: refactor
+
+    const currentTop = this.state.position.top;
+    const currentLeft = this.state.position.left;
+    const { direction } = this.state;
+    const { step, border, size, topScoreBoardHeight } = this.props;
+
+    if (direction === 'up') {
+      this.setState({
+        position: {
+          // top: currentTop - step,
+          top: Math.max(currentTop - step, 0),
+          left: currentLeft
+        }
+      });
+    } else if (direction === 'right') {
+      this.setState({
+        position: {
+          top: currentTop,
+          // left: currentLeft + step
+          left: Math.min(currentLeft + step, window.innerWidth - border - size)
+        }
+      });
+    } else if (direction === 'down') {
+      this.setState({
+        position: {
+          // top: currentTop + step,
+          top: Math.min(currentTop + step, window.innerHeight - border - size - topScoreBoardHeight),
+          left: currentLeft
+        }
+      });
+    } else if (direction === 'left') {
+      this.setState({
+        position: {
+          top: currentTop,
+          // left: currentLeft - step
+          left: Math.max(currentLeft - step, 0)
+        }
+      });
+    }
+
   }
 
   render() {
