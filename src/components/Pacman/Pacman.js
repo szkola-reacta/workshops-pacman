9234f71f6cb0c3e762417ba5627820bbe762a03f
patrykomiotek <patryk.omiotek@gmail.com>
2020-09-26 11:13:04 +0200

    pacman as svg

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
+import { ReactComponent as PacmanSvg } from './pacman.svg';
+import './style.css';
 class Pacman extends Component {
+
+  state = {
+    direction: 'right',
+    position: {
+      top: 50,
+      left: 50
+    }
+  }
+
   render() {
     return (
-      <div className="pacman">
-        Pacman
+      <div
+        className="pacman"
+        style={this.state.position}
+      >
+        <PacmanSvg />
       </div>
     );
   }
+}
+
+Pacman.defaultProps = {
+  step: 50, // 50px
+  size: 50, // pacman size: 50x50px
+  // TODO: move to config
+  border: 10 * 2,
+  topScoreBoardHeight: 50
 }
 
 export default Pacman;
